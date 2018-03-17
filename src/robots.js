857fd663968cf45e6a7d7551d3c9faf2529d81d1
magvin <zans.litinskis@gmail.com>
2018-03-17 00:42:42 +0000

    first commit

---
+++ src/robots.js
@@ -0,0 +1,50 @@
+export const robots =[
+{
+	id:1,
+	name: "Marco Polo",
+	username: 'Mary',
+	email: 'littlepolo@april.biz'
+},
+{
+	id:2,
+	name: "Etnaa Slow",
+	username: 'Tony',
+	email: 'TonyStark@april.biz'
+},
+{
+	id:3,
+	name: "Juanes Garcys Granados",
+	username: 'GG',
+	email: 'ggj@april.biz'
+},
+{
+	id:4,
+	name: "MaKaty Moly",
+	username: 'Kate',
+	email: 'KateMoly@april.biz'
+},
+{
+	id:5,
+	name: "Frost Weather",
+	username: 'Freeze',
+	email: 'bettersun@april.biz'
+},
+{
+	id:6,
+	name: "Marco Polo",
+	username: 'Mary',
+	email: 'littlepolo@april.biz'
+},
+{
+	id:7,
+	name: "Reg Mi",
+	username: 'Michael',
+	email: 'noacc@april.biz'
+},
+{
+	id:8,
+	name: "Wheard Dreed",
+	username: 'Dead',
+	email: 'DreadDead@april.biz'
+},
+]
